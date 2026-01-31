4685c6257bd9a6608252706f7a554c3608039b20
Arindam saikia <arindamsaikia50a@gmail.com>
2026-01-31 13:12:02 +0530

    add payment page

---
+++ app/(pages)/courses/[1]/checkout/page.tsx
@@ -0,0 +1,61 @@
+export default function Page() {
+  return (
+    <div className="bg-[#f9fafb] min-h-screen w-full p-5 md:p-10">
+      <div className="mx-auto max-w-7xl w-full">
+        <div className="flex flex-wrap justify-center gap-6 w-full">
+
+          {/* Course Card */}
+          <div className="bg-white rounded-2xl flex flex-col border border-gray-200 p-5 gap-4 shadow-sm w-full md:w-[48%]">
+            <img
+              className="w-full h-55 md:h-[305px] rounded-2xl object-cover"
+              src="/course-image.jpg"
+              alt="course-image"
+            />
+
+            <h3 className="text-2xl font-bold tracking-tight text-[#001959]">
+              100xSchool Combined Bootcamp
+            </h3>
+
+            <p className="text-gray-600">Web dev (Every Friday)</p>
+            <p className="text-gray-600">Devops (Every Friday)</p>
+            <p className="text-gray-600">...</p>
+          </div>
+
+          {/* Payment Method */}
+          <div className="bg-white rounded-2xl border border-gray-200 p-6 w-full md:w-[48%] space-y-5">
+            <h3 className="text-xl font-bold text-[#001959]">
+              Purchase Details
+            </h3>
+
+            <div className="space-y-4">
+              <div className="flex justify-between">
+                <p className="text-gray-600">Payment Currency</p>
+                <p className="text-[#001959] font-medium">INR</p>
+              </div>
+
+              <div className="flex justify-between">
+                <p className="text-gray-600">Price (Including GST)</p>
+                <p className="text-[#001959] font-medium">₹5,999</p>
+              </div>
+
+              <div className="flex justify-between">
+                <p className="text-gray-600">Net Discount</p>
+                <p className="text-[#001959] font-medium">₹0</p>
+              </div>
+
+              <div className="flex justify-between items-center">
+                <p className="text-[#001959] font-semibold">Total</p>
+                <p className="text-[#001959] text-2xl font-bold">₹5,999</p>
+              </div>
+            </div>
+
+            <button className="bg-[#001959] hover:bg-[#001540] transition px-4 py-3 w-full text-white rounded-2xl font-semibold">
+              Buy Now
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
